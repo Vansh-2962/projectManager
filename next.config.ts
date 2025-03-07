c379a39d566e7ab61f5c63bd86da954463ac435a
Vansh singh sura <vanshsura29@gmail.com>
2025-03-07 12:34:29 +0530

    bug fix

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  env: {
+    CLERK_WEBHOOK_SECRET: process.env.CLERK_WEBHOOK_SECRET,
+  },
 };
 
 export default nextConfig;
